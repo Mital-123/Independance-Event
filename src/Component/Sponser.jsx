5d1d33cd5915bc164b005635525a1d2b5e5b0ac7
Mital-123 <mailto:mittalvaghela2003@gmail.com>
2025-07-23 15:14:31 +0530

    background image change in home and every small changes

--- src/Component/Sponser.jsx
+++ src/Component/Sponser.jsx
@@ -20,7 +20,7 @@ function Sponser() {
 
       <div className="container px-3">
         {/* Header Section */}
-        <div className="row justify-content-center mb-4">
+        <div className="row justify-content-center mb-3">
           <div className="col-lg-8 text-center">
             <div className="header-content">
               <h2
@@ -39,75 +39,79 @@ function Sponser() {
         </div>
 
         {/* Logos Section in Row/Cols */}
-        <div className="row g-4 justify-content-center">
+        <div className="">
           {/* Item 1 */}
-          <div className="col-6 col-md-4 col-lg-2" data-aos="zoom-in" data-aos-duration="1000">
-            <div className="certificate-item h-100 d-flex align-items-center justify-content-center">
-              <img
-                src="https://baprerna.com/_next/image?url=%2Fassets%2Ffavicon.png&w=384&q=75"
-                alt="Sponsor 1"
-                className="img-fluid"
-              />
-              <div className="certificate-glow"></div>
+          <div className="row g-4 justify-content-center mb-3">
+            {/* Logo 1 */}
+            <div className="col-12 col-md-3 col-lg-2 d-flex justify-content-center" data-aos="zoom-in" data-aos-duration="1000">
+              <div className="certificate-item d-flex align-items-center justify-content-center">
+                <img
+                  src="https://baprerna.com/_next/image?url=%2Fassets%2Ffavicon.png&w=384&q=75"
+                  alt="Sponsor 1"
+                  className="img-fluid"
+                />
+                <div className="certificate-glow"></div>
+              </div>
+            </div>
+
+            {/* Logo 2 */}
+            <div className="col-12 col-md-3 col-lg-2 d-flex justify-content-center" data-aos="zoom-in" data-aos-duration="1000">
+              <div className="certificate-item d-flex align-items-center justify-content-center">
+                <img
+                  src={require("../assets/images/WhatsApp Image 2025-07-22 at 16.03.34_259c768c.jpg")}
+                  alt="Sponsor 2"
+                  className="img-fluid"
+                />
+                <div className="certificate-glow"></div>
+              </div>
             </div>
           </div>
 
-          {/* Item 2 */}
-          <div className="col-6 col-md-4 col-lg-2" data-aos="zoom-in" data-aos-duration="1000">
-            <div className="certificate-item h-100 d-flex align-items-center justify-content-center">
-              <img
-                src={require("../assets/images/WhatsApp Image 2025-07-22 at 16.03.34_259c768c.jpg")}
-                alt="Sponsor 2"
-                className="img-fluid"
-              />
-              <div className="certificate-glow"></div>
-            </div>
-          </div>
 
           {/* Item 3 */}
-          <div className="col-6 col-md-4 col-lg-2" data-aos="zoom-in" data-aos-duration="1500" data-aos-delay="200">
-            <div className="certificate-item h-100 d-flex align-items-center justify-content-center">
-              <img
-                src={require("../assets/images/sampada logo.png")}
-                alt="Sponsor 3"
-                className="img-fluid"
-              />
-              <div className="certificate-glow"></div>
+          <div className="row g-4 justify-content-center">
+            <div className="col-6 col-md-3 col-lg-2 d-flex justify-content-center" data-aos="zoom-in" data-aos-duration="1500" data-aos-delay="200">
+              <div className="certificate-item h-100 d-flex align-items-center justify-content-center">
+                <img
+                  src={require("../assets/images/sampada logo.png")}
+                  alt="Sponsor 3"
+                  className="img-fluid"
+                />
+                <div className="certificate-glow"></div>
+              </div>
             </div>
-          </div>
 
-          {/* Item 4 */}
-          <div className="col-6 col-md-4 col-lg-2" data-aos="zoom-in" data-aos-duration="1500" data-aos-delay="200">
-            <div className="certificate-item h-100 d-flex align-items-center justify-content-center">
-              <img
-                src={require("../assets/images/city tadka logo.png")}
-                alt="Sponsor 4"
-                className="img-fluid"
-              />
-              <div className="certificate-glow"></div>
+            <div className="col-6 col-md-3 col-lg-2 d-flex justify-content-center" data-aos="zoom-in" data-aos-duration="1500" data-aos-delay="200">
+              <div className="certificate-item h-100 d-flex align-items-center justify-content-center">
+                <img
+                  src={require("../assets/images/city tadka logo.png")}
+                  alt="Sponsor 4"
+                  className="img-fluid"
+                />
+                <div className="certificate-glow"></div>
+              </div>
             </div>
-          </div>
 
-          {/* Item 5 */}
-          <div className="col-6 col-md-4 col-lg-2" data-aos="zoom-in" data-aos-duration="1500" data-aos-delay="200">
-            <div className="certificate-item h-100 d-flex align-items-center justify-content-center">
-              <img
-                src={require("../assets/images/arjiv-exports.4ad8dfcfc8b6bac62735.png")}
-                alt="Sponsor 5"
-                className="img-fluid"
-              />
-              <div className="certificate-glow"></div>
+            <div className="col-6 col-md-3 col-lg-2 d-flex justify-content-center" data-aos="zoom-in" data-aos-duration="1500" data-aos-delay="200">
+              <div className="certificate-item h-100 d-flex align-items-center justify-content-center">
+                <img
+                  src={require("../assets/images/arjiv-exports.4ad8dfcfc8b6bac62735.png")}
+                  alt="Sponsor 5"
+                  className="img-fluid"
+                />
+                <div className="certificate-glow"></div>
+              </div>
             </div>
-          </div>
 
-          <div className="col-6 col-md-4 col-lg-2" data-aos="zoom-in" data-aos-duration="1500" data-aos-delay="200">
-            <div className="certificate-item h-100 d-flex align-items-center justify-content-center">
-              <img
-                src={require("../assets/images/Screenshot_22-7-2025_153612_.jpeg")}
-                alt="Sponsor 5"
-                className="img-fluid"
-              />
-              <div className="certificate-glow"></div>
+            <div className="col-6 col-md-3 col-lg-2 d-flex justify-content-center" data-aos="zoom-in" data-aos-duration="1500" data-aos-delay="200">
+              <div className="certificate-item h-100 d-flex align-items-center justify-content-center">
+                <img
+                  src={require("../assets/images/Screenshot_22-7-2025_153612_.jpeg")}
+                  alt="Sponsor 6"
+                  className="img-fluid"
+                />
+                <div className="certificate-glow"></div>
+              </div>
             </div>
           </div>
 
